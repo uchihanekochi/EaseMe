48c185bf72f80d0802e56946674bfe6406097c6a
Khoi Map <88872386+MinhKhoiMap@users.noreply.github.com>
2023-05-27 19:16:53 +0700

    Update Book.js

--- components/minhkhoi/Book/Book.js
+++ components/minhkhoi/Book/Book.js
@@ -84,7 +84,7 @@ const Book = ({ pageContentArr = [], nameOfUser = "Phạm Minh Khôi" }) => {
                 <div className="content__body">
                   <p>
                     <span style={{ fontSize: "5rem" }}>
-                      {pageContentArr[i].content[0][0]}
+                      {pageContentArr[i].content[0][0][0]}
                     </span>
                     {pageContentArr[i].content[0].substring(
                       1,
